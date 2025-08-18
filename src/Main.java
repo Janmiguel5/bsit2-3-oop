bc9e7db8d4eec1f8816e0bac7a4d173d1e8a9a82
unknown <jinciso48428@liceo.edu.ph>
2025-08-18 15:57:10 +0800

    lab 4

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         GradeCalculator calculator = new GradeCalculator();
 
-        String studentName = "Sam Smith";
+        String studentName = "Sam Smiths";
         double[] grades = {85.5, 92.0, 78.5, 90.0};
 
         double average = calculator.calculateAverage(grades);
