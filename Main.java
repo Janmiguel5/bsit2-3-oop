3497635d41726361548e1bf351372f653d15af80
JanMiguelInciso <jinciso48428@liceo.edu.ph>
2025-09-11 01:08:50 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TotalNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int totalNumber = 0; 
+
+        
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter number " + i + ": ");
+            int number = input.nextInt();
+            totalNumber += number;       
+        }
+
+       
+        System.out.println("\nTotal Number: " + totalNumber);
+
+        input.close();
+    }
+}
